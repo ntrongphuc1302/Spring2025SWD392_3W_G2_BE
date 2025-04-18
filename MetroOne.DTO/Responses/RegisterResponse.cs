df736cb3dbe0e732880cc28715f434a0cdeb43d9
ntrongphuc1302 <ntrongphuc.1302@gmail.com>
2025-04-18 12:21:50 +0700

    Patch port problem

---
+++ MetroOne.DTO/Responses/RegisterResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroOne.DTO.Responses
+{
+    public class RegisterResponse
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = "Passenger";
+    }
+}
